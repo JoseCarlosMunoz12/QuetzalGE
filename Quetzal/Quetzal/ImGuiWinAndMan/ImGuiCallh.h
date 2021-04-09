89a75bead144c3033af0f275cedd71c30aca9e2b
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-04-09 12:53:34 -0500

    Added the widget library. test to see how it work. And also get framebuffer

--- Quetzal/Quetzal/ImGuiWinAndMan/ImGuiCallh.h
+++ Quetzal/Quetzal/ImGuiWinAndMan/ImGuiCallh.h
@@ -2,6 +2,10 @@
 #include "ImGui/imgui.h"
 #include "ImGui/imgui_impl_glfw.h"
 #include "ImGui/imgui_impl_opengl3.h"
+#include "ImGui/ImGuizmo.h"
+#include "ImGui/ImGuizmo.h"
+#include "ImGui/ImSequencer.h"
+#include "ImGui/ImZoomSlider.h"
 #include <glew.h>
 #include <glfw3.h>
 #include <glm.hpp>
