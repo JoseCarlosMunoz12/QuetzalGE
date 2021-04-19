c1b80438a42bdd6276940f04b806f039ca38b9c0
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-04-19 08:53:00 -0500

    Remove unnecessary data points in the Model Class

--- Quetzal/Quetzal/Renderer/Model.cpp
+++ Quetzal/Quetzal/Renderer/Model.cpp
@@ -1,28 +1,22 @@
 #include "Model.h"
 
 Model::Model(std::string NewName)
-	:Position(glm::vec3(0.f)), Rotation(Quat()), Scale(glm::vec3(1.f))
+	:Position(glm::vec3(0.f))
 {
 	this->Name = NewName;
 }
 
-Model::Model(std::string NewName,
-	glm::vec3 InitPos, Quat InitRot, glm::vec3 InitScale)
+Model::Model(std::string NewName,glm::vec3 InitPos)
 {
 	this->Name = NewName;
 	this->Position = InitPos;
-	this->Rotation = InitRot;
-	this->Scale = InitScale;
 }
 
-Model::Model(std::string NewName,
-	glm::vec3 InitPos, Quat InitRot, glm::vec3 InitScale,
+Model::Model(std::string NewName,glm::vec3 InitPos,
 	std::vector<std::shared_ptr<Mesh>> Meshes, std::vector<std::shared_ptr<Texture>> Textures, std::vector<std::shared_ptr<Material>> Materials)
 {
 	this->Name = NewName;
 	this->Position = InitPos;
-	this->Rotation = InitRot;
-	this->Scale = InitScale;
 	this->Meshes_Inf = Meshes;
 	this->Textures_Inf = Textures;
 	this->Materials_Inf = Materials;
--- Quetzal/Quetzal/Renderer/Model.h
+++ Quetzal/Quetzal/Renderer/Model.h
@@ -16,27 +16,19 @@ private:
 	std::vector<std::shared_ptr<Node>> Nodes_Inf;
 	//Location, orientation and scale of whole model
 	glm::vec3 Position;
-	Quat Rotation;
-	glm::vec3 Scale;
 	std::string Name;
 	//Holds Physics, Collision information of the Model
 public:
 	Model(std::string NewName);
-	Model(std::string NewName,
-		glm::vec3 InitPos, Quat InitRot, glm::vec3 InitScale);
-	Model(std::string NewName,
-		glm::vec3 InitPos, Quat InitRot, glm::vec3 InitScale,
+	Model(std::string NewName,glm::vec3 InitPos);
+	Model(std::string NewName,glm::vec3 InitPos,
 		std::vector<std::shared_ptr<Mesh>> Meshes, std::vector<std::shared_ptr<Texture>> Textures, std::vector<std::shared_ptr<Material>> Materials);
 
 	~Model() {}
 	//Setters
-	void SetPos(glm::vec3 NewPos)     { this->Position = NewPos; }
-	void SetRot(Quat NewRot)          { this->Rotation = NewRot; }
-	void SetScale(glm::vec3 NewScale) { this->Scale = NewScale; }
+	void SetPos(glm::vec3 NewPos) { this->Position = NewPos; }
 	//Getters
 	glm::vec3 GetPos()    { return this->Position; }
-	Quat GetRot()         { return this->Rotation; }
-	glm::vec3 GetScale()  { return this->Scale; }
 	std::string GetName() { return this->Name; }
 	//Render Model
 };
