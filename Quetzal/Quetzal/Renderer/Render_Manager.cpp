a634519eb0b0e184088549a9015f4d1420b212d8
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-04-20 09:10:59 -0500

    Prepared to make a first render to a box

--- Quetzal/Quetzal/Renderer/Render_Manager.cpp
+++ Quetzal/Quetzal/Renderer/Render_Manager.cpp
@@ -1,1 +1,9 @@
 #include "Render_Manager.h"
+
+void Render_Manager::Update(float dt)
+{
+}
+
+void Render_Manager::Render()
+{
+}
--- Quetzal/Quetzal/Renderer/Render_Manager.h
+++ Quetzal/Quetzal/Renderer/Render_Manager.h
@@ -4,8 +4,16 @@
 class Render_Manager
 {
 private:
-	std::vector<std::shared_ptr<Shader>> Shader_All;
+	std::vector<std::shared_ptr<Shader>> All_Shader;
+	std::vector<std::shared_ptr<Texture>> All_Texture;
+	std::vector<std::shared_ptr<Mesh>> All_Meshes;
+	std::vector<std::shared_ptr<Material>> All_Materials;
+	std::vector<std::shared_ptr<Model>> All_Models;
 public:
 	Render_Manager() {}
+	//Updates Positions, animations for all Models in the world
+	void Update(float dt);
+	//Renders all
+	void Render();
 };
 
