b7070398c0b996873703137dda347bc4a9dcbcd9
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-04-22 09:18:00 -0500

    Set init for camera

--- Quetzal/Quetzal/Renderer/Camera.cpp
+++ Quetzal/Quetzal/Renderer/Camera.cpp
@@ -1,10 +1,30 @@
 #include "Camera.h"
 
 
+void Camera::UpdateCameraVectors()
+{
+
+	this->front.x = -cos(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+	this->front.z = sin(glm::radians(this->pitch));
+	this->front.y = sin(glm::radians(this->yaw)) * cos(glm::radians(this->pitch));
+
+	this->front = glm::normalize(this->front);
+	this->right = glm::normalize(glm::cross(this->front, this->worldUp));
+	this->up = glm::normalize(glm::cross(this->right, this->front));
+}
+
 Camera::Camera(glm::vec3 Pos, glm::vec3 WorldUp,
 	float InitMovement, float InitSensitivity)
-	:position(Pos),worldUp(WorldUp),movementSpeed(InitMovement), sensitivity(InitSensitivity)
+	:position(Pos),worldUp(WorldUp),up(WorldUp),
+	movementSpeed(InitMovement), sensitivity(InitSensitivity)
 {
+	this->ViewMatrix = glm::mat4(1.f);
+	this->right = glm::vec3(0.f);
+	this->up = worldUp;
+	this->pitch = 0.f;
+	this->yaw = 0.f;
+	this->roll = 0.f;
+	this->UpdateCameraVectors();
 }
 
 void Camera::Update(float dt, const int Direc)
--- Quetzal/Quetzal/Renderer/Camera.h
+++ Quetzal/Quetzal/Renderer/Camera.h
@@ -36,6 +36,7 @@ private:
 	GLfloat roll;
 
 	glm::mat4 ViewMatrix;
+	void UpdateCameraVectors();
 public:
 	Camera(glm::vec3 Pos, glm::vec3 WorldUp,
 		float InitMovement = 3.f, float InitSensitivity = 5.f);
@@ -43,10 +44,12 @@ public:
 	//Getters
 	glm::vec3 GetPos() { return this->position; }
 	glm::mat4 GetViewMatrix() { return this->ViewMatrix; }
-	//Setters
-	void SetPosition(glm::vec3 NewPos) { this->position = NewPos; }
 	float GetMovementSpeed()           { return this->movementSpeed; }
 	float GetSensitivity()             { return this->sensitivity; }
+	//Setters
+	void SetPosition(glm::vec3 NewPos)  { this->position = NewPos; }
+	void SetMovement(float NewMv)       { this->movementSpeed = NewMv; }
+	void SetSensitivity(float NewSense) { this->sensitivity = NewSense; }
 	//update matrices
 	void Update(float dt, const int Direc);
 };
