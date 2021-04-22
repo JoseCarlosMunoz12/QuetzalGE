5f1dfed9b793af3553be0bfccca30c70083a0b71
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-04-22 09:02:35 -0500

    adding parts for the camera and able to get items to edit

--- Quetzal/Quetzal/Renderer/Camera.cpp
+++ Quetzal/Quetzal/Renderer/Camera.cpp
@@ -1,1 +1,13 @@
 #include "Camera.h"
+
+
+Camera::Camera(glm::vec3 Pos, glm::vec3 WorldUp,
+	float InitMovement, float InitSensitivity)
+	:position(Pos),worldUp(WorldUp),movementSpeed(InitMovement), sensitivity(InitSensitivity)
+{
+}
+
+void Camera::Update(float dt, const int Direc)
+{
+	Direc;
+}
--- Quetzal/Quetzal/Renderer/Camera.h
+++ Quetzal/Quetzal/Renderer/Camera.h
@@ -10,7 +10,7 @@
 #include <gtc\type_ptr.hpp>
 #include "Render_Items/Vertex.h"
 
-enum direction {
+enum class direction {
 	FORWARD = 0,
 	BACKWARD,
 	LEFT,
@@ -20,5 +20,33 @@ enum direction {
 };
 class Camera
 {
-};
+private:
 
+	GLfloat movementSpeed;
+	GLfloat sensitivity;
+
+	glm::vec3 worldUp;
+	glm::vec3 position;
+	glm::vec3 front;
+	glm::vec3 right;
+	glm::vec3 up;
+
+	GLfloat pitch;
+	GLfloat yaw;
+	GLfloat roll;
+
+	glm::mat4 ViewMatrix;
+public:
+	Camera(glm::vec3 Pos, glm::vec3 WorldUp,
+		float InitMovement = 3.f, float InitSensitivity = 5.f);
+	~Camera() {}
+	//Getters
+	glm::vec3 GetPos() { return this->position; }
+	glm::mat4 GetViewMatrix() { return this->ViewMatrix; }
+	//Setters
+	void SetPosition(glm::vec3 NewPos) { this->position = NewPos; }
+	float GetMovementSpeed()           { return this->movementSpeed; }
+	float GetSensitivity()             { return this->sensitivity; }
+	//update matrices
+	void Update(float dt, const int Direc);
+};
