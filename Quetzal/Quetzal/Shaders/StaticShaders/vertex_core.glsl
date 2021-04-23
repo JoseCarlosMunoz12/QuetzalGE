56c09a98cd067fb83fad53681fc4e162dd306d31
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-04-23 12:26:25 -0500

    Fix the camera issue with the wrong direction and incorrect location

--- Quetzal/Quetzal/Shaders/StaticShaders/vertex_core.glsl
+++ Quetzal/Quetzal/Shaders/StaticShaders/vertex_core.glsl
@@ -18,9 +18,7 @@ void main()
 {
 	vs_position = vec4(ModelMatrix * vec4(vertex_position, 1.f)).xyz;
 	vs_color = vertex_color;
-	vec4 positionRelativeToCam = ViewMatrix * ModelMatrix * vec4(vertex_position, 1.f);
-	vs_texcoord = vec2(vertex_texcoord.x, vertex_texcoord.y *-1.f);
+	vs_texcoord = vec2(vertex_texcoord.x, vertex_texcoord.y);
 	vs_normal = mat3(ModelMatrix) * vertex_normal;
-
 	gl_Position = ProjectionMatrix * ViewMatrix * ModelMatrix * vec4(vertex_position, 1.f);
 }
