2b05e7038a5e13d9c6c14b15589792ce4a9e5818
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-04-23 06:59:52 -0500

    Fixing the framebuffer creation

--- Quetzal/Quetzal/Renderer/Render_Items/Texture.h
+++ Quetzal/Quetzal/Renderer/Render_Items/Texture.h
@@ -30,7 +30,6 @@ public:
 	{
 		return this->Name;
 	}
-
 	inline GLuint getID() const { return this->ID; }
 };
 
@@ -137,6 +136,7 @@ class Frame_Buffer : public Texture
 {
 private:
 	GLuint FrameBuffer;
+	GLuint rbo;
 public:
 	Frame_Buffer(const char* name)
 		:Texture(name,GL_TEXTURE_2D)
@@ -157,6 +157,15 @@ public:
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, this->ID, 0);
+		// create a renderbuffer object for depth and stencil attachment (we won't be sampling these)
+		glGenRenderbuffers(1, &this->rbo);
+		glBindRenderbuffer(GL_RENDERBUFFER, this->rbo);
+		glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, WindowWidth, WindowHeight); // use a single renderbuffer object for both a depth AND stencil buffer.
+		glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, this->rbo); // now actually attach it
+		// now that we actually created the framebuffer and added all attachments we want to check if it is actually complete now
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+			std::cout << "ERROR::FRAMEBUFFER:: Framebuffer is not complete!\n";
+		glBindFramebuffer(GL_FRAMEBUFFER, 0);
 	}
 	void WriteToBuffer(unsigned int WindowWidth, unsigned int WindowHeight,
 		std::shared_ptr<Shader> DepthShader, glm::mat4 CamMatrix)
