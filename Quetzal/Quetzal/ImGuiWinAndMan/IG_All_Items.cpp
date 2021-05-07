1a61c32489d0dd31ae07bebd16b53936f640823f
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-05-07 13:14:41 -0500

    using only quat Parts for rotation

--- Quetzal/Quetzal/ImGuiWinAndMan/IG_All_Items.cpp
+++ Quetzal/Quetzal/ImGuiWinAndMan/IG_All_Items.cpp
@@ -12,8 +12,6 @@ void IG_All_Items::DisplayChildren(Vec_SH<Node> Chld)
 IG_All_Items::IG_All_Items(std::string Name)
 	:WinName(Name)
 {
-    this->ERs = glm::vec3(0.f, 90.f, 0.f);
-    this->QRs = glm::quat((glm::pi<float>() / 180.f) * ERs);
 }
 
 IG_All_Items::~IG_All_Items()
@@ -36,18 +34,6 @@ void IG_All_Items::Update()
             {
                 if (ImGui::TreeNode(ii->GetName().c_str()))
                 {
-                    this->ERs = glm::eulerAngles(this->QRs);
-                    this->ERs = (180.f / glm::pi<float>()) * this->ERs;
-                    ImGui::Text("General information");
-                    if (ImGui::SliderFloat("X-Axis", &ERs.x, -180, 180))
-                        this->QRs = glm::quat((glm::pi<float>() / 180.f) * this->ERs);
-                    if (ImGui::SliderFloat("Y-Axis", &ERs.y, -180, 180))
-                    {
-                        this->ERs = (glm::pi<float>() / 180.f) * this->ERs;
-                        this->QRs = glm::quat(this->ERs);
-                    }
-                    if (ImGui::SliderFloat("Z-Axis", &ERs.z, -180, 180))
-                        this->QRs = glm::quat((glm::pi<float>() / 180.f) * this->ERs);
                     //this->DisplayChildren(Nd->GetChildren());
                     ImGui::TreePop();
                 }
--- Quetzal/Quetzal/ImGuiWinAndMan/IG_All_Items.h
+++ Quetzal/Quetzal/ImGuiWinAndMan/IG_All_Items.h
@@ -5,8 +5,6 @@ class IG_All_Items : public ImGuiWinPar
 {
 private:
 	std::string WinName;
-	glm::quat QRs;
-	glm::vec3 ERs;
 	void DisplayChildren(Vec_SH<Node> Chld);
 public:
 	IG_All_Items(std::string Name = "All Items");
