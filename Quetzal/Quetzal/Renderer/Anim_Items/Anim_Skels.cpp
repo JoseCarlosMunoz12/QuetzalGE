5b414007f561241e983dc2b054878bb03603387b
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-05-11 08:18:02 -0500

    Set Getters for frames and constructor

--- Quetzal/Quetzal/Renderer/Anim_Items/Anim_Skels.cpp
+++ Quetzal/Quetzal/Renderer/Anim_Items/Anim_Skels.cpp
@@ -1,1 +1,59 @@
 #include "Anim_Skels.h"
+
+Frames::Frames(float InitTime, Joint InitJoint, InterType InitType)
+{
+    this->Time_Stamp = InitTime;
+    this->Jnt = InitJoint;
+    this->Type = InitType;
+    this->Bias = {0.f,0.f};
+}
+
+glm::quat Frames::GetRot()
+{
+    return this->Jnt.Rotation;
+}
+
+glm::vec3 Frames::GetOffset()
+{
+    return this->Jnt.Offset;
+}
+
+glm::vec3 Frames::GetScale()
+{
+    return this->Jnt.Scale;
+}
+
+Bezier_Bais Frames::GetBezier()
+{
+    return this->Bias;
+}
+
+InterType Frames::GetType()
+{
+    return this->Type;
+}
+
+float Frames::GetTimeStamp()
+{
+    return this->Time_Stamp;
+}
+
+void Frames::SetRot(glm::quat NewRot)
+{
+}
+
+void Frames::SetOffset(glm::vec3 NewOffset)
+{
+}
+
+void Frames::SetBezier(Bezier_Bais NewBias)
+{
+}
+
+void Frames::SetType(InterType NewType)
+{
+}
+
+Anim_Skels::Anim_Skels()
+{
+}
--- Quetzal/Quetzal/Renderer/Anim_Items/Anim_Skels.h
+++ Quetzal/Quetzal/Renderer/Anim_Items/Anim_Skels.h
@@ -3,8 +3,17 @@
 #include <vector>
 
 #include "../Render_Items/Vertex.h"
-#include "../Render_Items/Shader.h"
-#include "../Render_Items/Texture.h"
+enum class InterType
+{
+	HOLD,
+	LINEAR,
+	QUADBENZ,
+	CUBEBENZ
+};
+struct Bezier_Bais {
+	float Point0;
+	float Point1;
+};
 
 struct Joint
 {
@@ -13,7 +22,33 @@ struct Joint
 	glm::vec3 Scale;
 };
 
+class Frames
+{
+private:
+	float Time_Stamp;
+	Joint Jnt;
+	InterType Type;
+	Bezier_Bais Bias;
+public:
+	Frames(float InitTime, Joint InitJoint, InterType InitType = InterType::LINEAR);
+	~Frames() {}
+	glm::quat GetRot();
+	glm::vec3 GetOffset();
+	glm::vec3 GetScale();
+	Bezier_Bais GetBezier();
+	InterType GetType();
+	float GetTimeStamp();
+	void SetRot(glm::quat NewRot);
+	void SetOffset(glm::vec3 NewOffset);
+	void SetBezier(Bezier_Bais NewBias);
+	void SetType(InterType NewType);
+};
+
 class Anim_Skels
 {
+private:
+
+public:
+	Anim_Skels();
 };
 
