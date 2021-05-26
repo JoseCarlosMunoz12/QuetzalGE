cfa2f70a379d51deb0db5e8b368e330665d537d8
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-05-25 22:10:02 -0500

    added functions to write for the animation handler

--- Quetzal/Quetzal/Renderer/Anim_Items/AnimHandler.cpp
+++ Quetzal/Quetzal/Renderer/Anim_Items/AnimHandler.cpp
@@ -1,1 +1,38 @@
 #include "AnimHandler.h"
+
+AnimHandler::AnimHandler()
+{
+}
+
+AnimHandler::AnimHandler(std::map<std::string, glm::mat4> InitOffsets, std::map<std::string, glm::mat4> TransMats, std::map<std::string, int> InitBoneId)
+{
+}
+
+AnimHandler::~AnimHandler()
+{
+}
+
+void AnimHandler::InitOffsets(std::map<std::string, glm::mat4> InitOffsets)
+{
+}
+
+void AnimHandler::InitTransMat(std::map<std::string, glm::mat4> InitTransMat)
+{
+}
+
+void AnimHandler::InitBoneId(std::map<std::string, int> InitBoneId)
+{
+}
+
+void AnimHandler::AddAnimation()
+{
+}
+
+void AnimHandler::ChangeAnim(int AnimId)
+{
+}
+
+std::vector<glm::mat4> AnimHandler::GetMatrices()
+{
+	return std::vector<glm::mat4>();
+}
--- Quetzal/Quetzal/Renderer/Anim_Items/AnimHandler.h
+++ Quetzal/Quetzal/Renderer/Anim_Items/AnimHandler.h
@@ -2,5 +2,24 @@
 #include "Animation.h"
 class AnimHandler
 {
+private:
+	//base data of the whole model for animation
+	std::map<std::string, glm::mat4> Offsets;
+	std::map<std::string, glm::mat4> TransMats;
+	std::map<std::string, int> BoneId;
+	std::vector<glm::mat4> AnimMats;
+	//Animation data and how it blends with each other
+	Vec_SH<Animation> Anims;
+public:
+	AnimHandler();
+	AnimHandler(std::map<std::string,glm::mat4> InitOffsets, std::map<std::string,glm::mat4>TransMats,
+		std::map<std::string,int> InitBoneId);
+	~AnimHandler();
+	void InitOffsets(std::map<std::string, glm::mat4> InitOffsets);
+	void InitTransMat(std::map<std::string, glm::mat4> InitTransMat);
+	void InitBoneId(std::map<std::string, int> InitBoneId);
+	void AddAnimation();
+	void ChangeAnim(int AnimId);
+	std::vector<glm::mat4> GetMatrices();
 };
 
