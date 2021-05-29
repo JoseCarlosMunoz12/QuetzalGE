54dbcee320430f6cfe109acfe9b28cb18f86f6ea
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-05-28 21:04:29 -0500

    Animation handler handles updates

--- Quetzal/Quetzal/Renderer/AnimHandler.cpp
+++ Quetzal/Quetzal/Renderer/AnimHandler.cpp
@@ -7,3 +7,14 @@ AnimHandler::AnimHandler()
 AnimHandler::~AnimHandler()
 {
 }
+
+void AnimHandler::AddAnims(M_S_M Offsets, M_S_M TransMats, M_S_I BoneLocs)
+{
+	this->AllAnims.push_back(std::make_shared<AnimationData>(Offsets, TransMats, BoneLocs));
+}
+
+void AnimHandler::Update(float dt)
+{
+	for (auto& jj : AllAnims)
+		jj->Update(dt);
+}
--- Quetzal/Quetzal/Renderer/AnimHandler.h
+++ Quetzal/Quetzal/Renderer/AnimHandler.h
@@ -3,7 +3,11 @@
 class AnimHandler
 {
 private:
+	Vec_SH<AnimationData> AllAnims;
 public:
 	AnimHandler();
 	~AnimHandler();
+	void AddAnims(M_S_M Offsets, M_S_M TransMats, M_S_I BoneLocs);
+	void Update(float dt);
+	S_P<AnimationData> GetAnim(int Id) { return this->AllAnims[Id]; };
 };
