6fc22e70c781fec6d15f1027c0f9aec238afaabb
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-06-04 07:47:23 -0500

    preparing Blend system to use

--- Quetzal/Quetzal/Renderer/Anim_Items/AnimationData.h
+++ Quetzal/Quetzal/Renderer/Anim_Items/AnimationData.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Animation.h"
+#include "Blend_Animation.h"
 class AnimationData
 {
 private:
---
+++ Quetzal/Quetzal/Renderer/Anim_Items/Blend_Animation.h
@@ -0,0 +1,7 @@
+#pragma once
+#include "Animation.h"
+
+class Blend_Animation
+{
+};
+
