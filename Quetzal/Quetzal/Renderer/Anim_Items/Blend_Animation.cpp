e30bba4df5afa09ddd0f7512ea3f14b7f9e8f4a3
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-06-04 08:47:11 -0500

    creating of init for maps and Animation Names

--- Quetzal/Quetzal/Renderer/Anim_Items/Blend_Animation.cpp
+++ Quetzal/Quetzal/Renderer/Anim_Items/Blend_Animation.cpp
@@ -1,1 +1,27 @@
 #include "Blend_Animation.h"
+
+Blend_Animation::Blend_Animation(std::string InitName, std::vector<std::string> InitAnimNames)
+	:BlendName(InitName)
+{
+	this->SetAnimsToUse(InitAnimNames);
+}
+
+Blend_Animation::Blend_Animation(std::string InitName, M_S_M BonesName, std::vector<std::string> InitAnimNames)
+	:BlendName(InitName)
+{
+	this->SetAnimsToUse(InitAnimNames);
+	//Creates Base ratios to be edited
+	for (auto& jj : BonesName)
+		BlendRatios[jj.first] = 1.f;
+}
+
+Blend_Animation::Blend_Animation(std::string InitName, M_S_F InitRatios, std::vector<std::string> InitAnimNames)
+	:BlendName(InitName), BlendRatios(InitRatios)
+{
+	this->SetAnimsToUse(InitAnimNames);
+}
+
+void Blend_Animation::SetAnimsToUse(std::vector<std::string> InitAnimNames)
+{
+	this->AnimNames = InitAnimNames;
+}
--- Quetzal/Quetzal/Renderer/Anim_Items/Blend_Animation.h
+++ Quetzal/Quetzal/Renderer/Anim_Items/Blend_Animation.h
@@ -1,7 +1,15 @@
 #pragma once
 #include "Animation.h"
-
+using M_S_F = std::map<std::string, float>;
 class Blend_Animation
 {
+private:
+	M_S_F BlendRatios;
+	std::string BlendName;
+	std::vector<std::string> AnimNames;
+public:
+	Blend_Animation(std::string InitName, std::vector<std::string> InitAnimNames);
+	Blend_Animation(std::string InitName, M_S_M BonesName, std::vector<std::string> InitAnimNames);
+	Blend_Animation(std::string InitName, M_S_F InitRatios, std::vector<std::string> InitAnimNames);
+	void SetAnimsToUse(std::vector<std::string> InitAnimNames);
 };
-
