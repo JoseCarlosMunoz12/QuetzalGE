565d9437e72110835603b55c932058825a7a8051
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-06-15 09:14:46 -0500

    Checks file too see if its a static or dynamic model

--- Quetzal/Quetzal/Renderer/Mdl_Ldr/Mdl_Ldr.cpp
+++ Quetzal/Quetzal/Renderer/Mdl_Ldr/Mdl_Ldr.cpp
@@ -1,1 +1,27 @@
 #include "Mdl_Ldr.h"
+
+void Mdl_Ldr::LoadFile(std::string FileName)
+{
+	File;
+	Assimp::Importer importer;
+	const aiScene* scene = importer.ReadFile(File + FileName, aiProcess_Triangulate | aiProcess_JoinIdenticalVertices | aiProcess_FlipUVs);
+	//Checks if file is valid or exists
+	if (!scene)
+	{
+		std::cout << "Error";
+		return;
+	}
+	//checks file to see if it has bones, to see if it's an A_Primitive
+	int MshNums = scene->mNumMeshes;
+	bool IsDynamic = false;
+	for (int ii = 0; ii < MshNums; ii++)
+		if (scene->mMeshes[ii]->HasBones())
+		{
+			IsDynamic = true;
+			break;
+		}
+	if (!IsDynamic)
+		this->CreateStatic();
+	else
+		this->CreateDynamic();
+}
--- Quetzal/Quetzal/Renderer/Mdl_Ldr/Mdl_Ldr.h
+++ Quetzal/Quetzal/Renderer/Mdl_Ldr/Mdl_Ldr.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "../Anim_Items/Anim_Primitive.h"
+#include "../Static_Items/Primitive.h"
 class Mdl_Ldr
 {
+private:
+	std::string File = "Models/ModelCol/";
+	//After Check, create the either dynamic or static Primitives to be used in the New Model
+	Vec_UP<Primitive> CreateStatic();
+	Vec_UP<A_Primitive> CreateDynamic();
+public:
+	Mdl_Ldr() {};
+	void LoadFile(std::string FileName);
+
 };
 
