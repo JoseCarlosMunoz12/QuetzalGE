646b1a8444a998091d4075fde09b08a0eeb0800b
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2021-09-21 16:17:03 -0500

    Loading the Q_Model also  changes the current vectors of items

--- Quetzal/Quetzal/Renderer/Mdl_Ldr/Q_Loader.cpp
+++ Quetzal/Quetzal/Renderer/Mdl_Ldr/Q_Loader.cpp
@@ -4,6 +4,6 @@ Q_Loader::Q_Loader()
 {
 }
 
-void Q_Loader::LoadQ_File(std::string FileName)
+void Q_Loader::LoadQ_File(std::string FileName, Vec_SH<Texture> Txts, Vec_SH<Shader> Shdrs, Vec_SH<Model>& Mdls, Vec_SH<Mesh>& Mshs, Vec_SH<Anim_Model>& A_Mdls, Vec_SH<Anim_Mesh>& A_Mshs, S_P<AnimHandler> AnimHndler)
 {
 }
--- Quetzal/Quetzal/Renderer/Mdl_Ldr/Q_Loader.h
+++ Quetzal/Quetzal/Renderer/Mdl_Ldr/Q_Loader.h
@@ -11,5 +11,7 @@ private:
 	
 public:
 	Q_Loader();
-	void LoadQ_File(std::string FileName);
+	void LoadQ_File(std::string FileName, Vec_SH<Texture> Txts, Vec_SH<Shader> Shdrs,
+		Vec_SH<Model>& Mdls, Vec_SH<Mesh>& Mshs,
+		Vec_SH<Anim_Model>& A_Mdls, Vec_SH<Anim_Mesh>& A_Mshs, S_P<AnimHandler> AnimHndler);
 };
