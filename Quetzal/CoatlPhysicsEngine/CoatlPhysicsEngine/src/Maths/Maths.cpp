94aa943bc3d397929924b175d23bfd51aa7b6f2e
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-02-08 16:32:26 -0600

    Have vec3

--- Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/Maths/Maths.cpp
+++ Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/Maths/Maths.cpp
@@ -3,9 +3,29 @@ using namespace CoatlPhysicsEngine;
 
 VectorXD::VectorXD(int initSize)
 {
+	for (int ii = 0; ii < initSize; ii++)
+		this->Vecs.push_back(0.f);
 }
 
-VectorXD::VectorXD(int initsize, double initVal)
+VectorXD::VectorXD(int initSize, double initVal)
+{
+	for (int ii = 0; ii < initSize; ii++)
+		this->Vecs.push_back(initVal);
+
+}
+
+Vector3D::Vector3D()
+	:VectorXD(3)
+{
+}
+
+Vector3D::Vector3D(double initVal)
+	: VectorXD(3, initVal)
+{
+}
+
+Vector3D::Vector3D(double initX, double initY, double initZ)
+	:VectorXD(3)
 {
 
 }
--- Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/Maths/Maths.h
+++ Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/Maths/Maths.h
@@ -5,10 +5,18 @@ namespace CoatlPhysicsEngine
 {
 	class COATL_API VectorXD
 	{
-	private:
+	protected:
 		std::vector<double> Vecs;
 	public:
 		VectorXD(int initSize);
 		VectorXD(int initSize, double initVal);
+	};
+	class COATL_API Vector3D : public VectorXD
+	{
+	private:
+	public:
+		Vector3D();
+		Vector3D(double initVal);
+		Vector3D(double initX, double initY, double initZ);
 	};
 }
