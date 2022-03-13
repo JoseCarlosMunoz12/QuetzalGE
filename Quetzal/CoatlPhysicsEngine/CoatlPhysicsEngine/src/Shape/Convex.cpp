8aefdb5fa1f8edf7701e7e3b4acdd1bd8af01b00
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-03-12 21:43:11 -0500

    Convex Shape is workin
    Convex shape can be called

--- Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/Shape/Convex.cpp
+++ Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/Shape/Convex.cpp
@@ -13,16 +13,24 @@ Convex::~Convex()
 
 void Convex::DisplayInfo()
 {
+	std::cout << "Shape Information Information:\n";
+	int size = this->Pnts.size();
+	printf("Number of Points : %d", size);
+	this->DisplayInternals();
 }
 
 std::string Convex::GetStr()
 {
-	return std::string();
+	std::string str = "Capsule Information:\n";
+	int size = this->Pnts.size();
+	str += "Number of Points: " + std::to_string(size) + "\n";
+	str += this->GetInternals();
+	return str;
 }
 
 std::vector<Vec3D> Convex::GetVertices()
 {
-	return std::vector<Vec3D>();
+	return this->Pnts;
 }
 
 std::vector<Vec3D> Convex::GetNormals()
@@ -32,7 +40,20 @@ std::vector<Vec3D> Convex::GetNormals()
 
 Vec3D Convex::Support(Vec3D dir)
 {
-	return Vec3D();
+	std::vector<Vec3D> Pnts = this->GetVertices();
+	double S = Pnts[0] * dir;
+	Vec3D MaxPnt = Pnts[0];
+	int Size = Pnts.size();
+	for (int ii = 1; ii < Size; ii++)
+	{
+		double T = Pnts[ii] * dir;
+		if (T > S)
+		{
+			S = T;
+			MaxPnt = Pnts[ii];
+		}
+	}
+	return MaxPnt;
 }
 
 Vec3D Convex::EPA_Support(Vec3D dir)
