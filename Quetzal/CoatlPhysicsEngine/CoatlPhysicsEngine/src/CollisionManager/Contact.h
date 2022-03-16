c1ef0e56b0c1e77a17f0ae37ae616b33739e3024
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-03-15 22:22:27 -0500

    Added parts of Contact points

--- Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/CollisionManager/Contact.h
+++ Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/CollisionManager/Contact.h
@@ -1,10 +1,18 @@
 #pragma once
 #include "../Shape/Shape.h"
+#include "../Body/Body.h"
 
  namespace CoatlPhysicsEngine {
 	 class Contact
 	 {
-
+	 private:
+		 S_P<Body> Bod0;
+		 S_P<Body> Bod1;
+		 Vec3D Pos;
+		 Vec3D Dir;
+		 double Penetration;
+	 public:
+		 Contact(S_P<Body> Bod0, S_P<Body> Bod1, Vec3D initPos, Vec3D initDir, double initPen);
 	 };
 
  }
