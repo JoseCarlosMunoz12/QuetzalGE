f89fd77ef4e8348282b1ffcc74ac44f98e6ebc6c
Jose Carlos Munoz <josecarlosmunoz12@gmail.com>
2022-06-17 19:04:36 -0500

    Moved Collision function to own class
    Collisoin to be used in world are in their own class. for easier understanding

--- Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/Collisions/BaseCols.cpp
+++ Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/Collisions/BaseCols.cpp
@@ -1,64 +1,6 @@
 #include "BaseCols.h"
 using namespace CoatlPhysicsEngine;
 
-bool BaseCols::UpdateBodies(S_P<Sphere> Sph0, S_P<Bodies> Bod0, S_P<Bodies> Bod1)
-{	
-	if (S_P<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod1->GetShapes()))
-	{
-		return CollisionManager::CheckCollide(Sph0, Sphere0,
-			Bod0->GetPos(),Bod0->GetQuat(),
-			Bod1->GetPos(), Bod1->GetQuat());
-	}
-	else if (S_P<Capsule> Cap = std::dynamic_pointer_cast<Capsule>(Bod1->GetShapes()))
-	{
-		return CollisionManager::CheckCollide(Sph0, Cap,Bod0->GetPos(),Bod0->GetQuat(), Bod1->GetPos(), Bod1->GetQuat());
-	}
-	return GJK_Alg::GJK(Bod1,Bod0);
-}
-
-bool BaseCols::UpdateBodies(S_P<Capsule> Cap0, S_P<Bodies> Bod0, S_P<Bodies> Bod1)
-{
-	if (S_P<Sphere> Sphere0 = std::dynamic_pointer_cast<Sphere>(Bod1->GetShapes()))
-	{
-		return CollisionManager::CheckCollide(Cap0, Sphere0,Bod0->GetPos(),Bod0->GetQuat(), Bod1->GetPos(), Bod1->GetQuat());
-	}
-	else if (S_P<Capsule> Cap = std::dynamic_pointer_cast<Capsule>(Bod1->GetShapes()))
-	{
-		return CollisionManager::CheckCollide(Cap0, Cap,Bod0->GetPos(),Bod0->GetQuat(), Bod1->GetPos(), Bod1->GetQuat());
-	}
-	return GJK_Alg::GJK(Bod1,Bod0);
-}
-
-bool BaseCols::ColBods(S_P<Bodies> Bod0, S_P<Bodies> Bod1)
-{
-	return GJK_Alg::GJK(Bod0, Bod1);
-}
-
-bool BaseCols::ColBods(S_P<Bodies> Bod0, S_P<Bodies> Bod1,
-	Vec<glm::vec3> Seg0, Vec<glm::vec3> Seg1)
-{
-	return GJK_Alg::GJK(Bod0, Bod1, Seg0, Seg1);
-}
-
-bool BaseCols::BinColDetection(S_P<Bodies> Bod0, S_P<Bodies> Bod1, glm::vec3 Vel0, glm::vec3 Vel1, float t0, float t1, float& NewDt)
-{
-	if ((t1 - t0) < EPSILON)
-	{
-		NewDt = t1;
-		return true;
-	}
-	float Mid = t0 + (t1 - t0) / 2.f;
-	if (!this->ColBods(Bod0, Bod1, { Vel0 * t0, Vel0 * t1 }, { Vel1 * t0, Vel1 * t1 }))
-	{
-		return false;
-	}
-	if (BinColDetection(Bod0, Bod1, Vel0, Vel1, t0, Mid, NewDt))
-	{
-		return true;
-	}
-	return BinColDetection(Bod0, Bod1, Vel0, Vel1, Mid, t1, NewDt);
-}
-
 BaseCols::BaseCols(std::string Name)
 {
 	this->Name = Name;
--- Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/Collisions/WorldCollisionHandler.h
+++ Quetzal/CoatlPhysicsEngine/CoatlPhysicsEngine/src/Collisions/WorldCollisionHandler.h
@@ -21,18 +21,19 @@
 #include "../OptimizaAlgorithm/B_Force_Self.h"
 #include "../GJK/GJK.h"
 #include "../CollisionResolution/ContactCreation.h"
-class WorldCollisionHandler
-{
-private:
-	const float EPSILON = 0.00015f;
-	bool UpdateBodies(S_P<Sphere> Sph0, S_P<Bodies> Bod0, S_P<Bodies> Bod1);
-	bool UpdateBodies(S_P<Capsule> Cap0, S_P<Bodies> Bod0, S_P<Bodies> Bod1);
-protected:
-	// other functions
-	bool ColBods(S_P<Bodies> Bod0, S_P<Bodies> Bod1);
-	bool ColBods(S_P<Bodies> Bod0, S_P<Bodies> Bod1, Vec<glm::vec3> Seg0, Vec<glm::vec3> S);
-	bool BinColDetection(S_P<Bodies> Bod0, S_P<Bodies> Bod1, glm::vec3 Vel0, glm::vec3 Vel1,
-		float t0, float t1, float& NewDt);
+namespace CoatlPhysicsEngine {
+	class WorldCollisionHandler
+	{
+	private:
+		const float EPSILON = 0.00015f;
+		bool UpdateBodies(S_P<Sphere> Sph0, S_P<Bodies> Bod0, S_P<Bodies> Bod1);
+		bool UpdateBodies(S_P<Capsule> Cap0, S_P<Bodies> Bod0, S_P<Bodies> Bod1);
+	protected:
+		// other functions
+		bool ColBods(S_P<Bodies> Bod0, S_P<Bodies> Bod1);
+		bool ColBods(S_P<Bodies> Bod0, S_P<Bodies> Bod1, Vec<glm::vec3> Seg0, Vec<glm::vec3> Seg1);
+		bool BinColDetection(S_P<Bodies> Bod0, S_P<Bodies> Bod1, glm::vec3 Vel0, glm::vec3 Vel1,
+			float t0, float t1, float& NewDt);
 
-};
-
+	};
+}
